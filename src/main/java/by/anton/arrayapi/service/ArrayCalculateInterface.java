d55922ed97dd655e3b9d6a9f34ce3d11509696e4
Anton Pysk <wertyal.87@mail.ru>
2021-03-29 08:11:10 +0300

    add *

--- src/main/java/by/anton/arrayapi/service/ArrayCalculateInterface.java
+++
@@ -1,9 +0,0 @@
-package by.anton.arrayapi.service;
-
-import by.anton.arrayapi.entity.CustomArray;
-import by.anton.arrayapi.exception.ApiException;
-
-public interface ArrayCalculateInterface {
-    int sumElementsOfArray(CustomArray customArray)throws ApiException;
-    int averageOfArray(CustomArray customArray)throws ApiException;
-}
--- src/main/java/by/anton/arrayapi/service/ArrayChangeInterface.java
+++
@@ -1,9 +0,0 @@
-package by.anton.arrayapi.service;
-
-import by.anton.arrayapi.entity.CustomArray;
-import by.anton.arrayapi.exception.ApiException;
-import by.anton.arrayapi.exception.IncorrectConditionException;
-
-public interface ArrayChangeInterface {
-    void changeElements(CustomArray customArray, String condition, int whatChange, int ch) throws ApiException, IncorrectConditionException;
-}
